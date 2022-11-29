4be488ddcd70b9263e0873fae7682ace9cb9e0bc
emadam <ldy9037@naver.com>
2022-11-29 09:03:56 +0900

    feat(myRoutes): circuitBreak에 대한 라우팅 설정 추가
    - 호스트가 *.circuitbreaker.com일 경우 circuitBreaker로 래핑 후 httpbin.org로 라우팅하도록 설정
    - 요청이 timeout 될 경우 fallback 메세지를 반환하도록 setFallbackUri와 /fallback RequestMapping 추가

--- src/main/java/com/emadam/gateway/GatewayApplication.java
+++ src/main/java/com/emadam/gateway/GatewayApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
+@RestController
 @SpringBootApplication
 public class GatewayApplication {
 
@@ -20,6 +25,17 @@ public class GatewayApplication {
 				.path("/get")
 				.filters(f -> f.addRequestHeader("Hello", "world"))
 				.uri("http://httpbin.org:80"))
+			.route(p -> p
+				.host("*.circuitbreaker.com")
+				.filters(f -> f.circuitBreaker(config -> config
+					.setName("mycmd")
+					.setFallbackUri("forward:/fallback")))
+				.uri("http://httpbin.org:80"))
 			.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
 	}
 }
